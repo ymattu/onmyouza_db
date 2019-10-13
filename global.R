31a96dabaa64b4f7149fff9ed90da5ac4cef548d
Yuya Matsumura <mattu.yuya@gmail.com>
2019-10-13 16:46:12 +0900

    セットリストの並び順を修正

--- global.R
+++ global.R
@@ -11,7 +11,11 @@ setlists_rds <- list.files(path = here::here('data'),
 df_dvd_songs <- read_rds('data/df_dvd_songs.rds') %>%
   filter(!str_detect(song, '収録'))
 df_setlists <- setlists_rds %>%
-  map_dfr(read_rds)
+  map_dfr(read_rds) %>%
+  ungroup() %>%
+  mutate(section = as.factor(section),
+         section = fct_relevel(section, "MAIN")) %>%
+  arrange(date, section, order)
 df_songs <- read_rds('data/df_songs.rds')
 
 dt_options <- list(lengthMenu = c(5, 10, 20, 50, 100, 200),
