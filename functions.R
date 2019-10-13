66a6300879431c1b44cb7210e67ec446e7156d9e
Yuya Matsumura <mattu.yuya@gmail.com>
2019-10-13 16:28:59 +0900

    update_setlist_data()を追加

--- functions.R
+++ functions.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(rvest)
+library(lubridate)
 
+#' Extract Sewtlist from a URL
+#' 
+#' @param  url URL
 extract_setlist <- function(url) {
   live <- url %>%
     read_html() %>%
@@ -48,4 +52,75 @@ extract_setlist <- function(url) {
     select(date, venue, section, order, song)
   
   return(df)
+}
+
+#' Update Setlist Data
+#' 
+update_setlist_data <- function() {
+  omz_bio <- "https://www.onmyo-za.net/biography/index.html"
+  df_setlists <- read_rds('data/df_setlists.rds')
+  db_latest <- df_setlists$date %>% tail(1)
+  omz_years <- year(df_setlists$date %>% tail(1) + days(1)):year(Sys.Date())
+  
+  # 活動履歴のURL一覧
+  rireki_years <- omz_years %>%
+    as.character() %>%
+    map_chr(~if_else(tail(.) == as.character(lubridate::year(Sys.Date())),
+                     "index",
+                     .))
+  rireki_base <- "https://www.onmyo-za.net/biography/"
+  rireki_urls <- rireki_years %>%
+    map_chr(~str_c(rireki_base,
+                   .,
+                   ".html"
+    ))
+  
+  message("Extracting Differences Between DB & Lives..")
+  # ライブ日程
+  live_dates <- rireki_urls %>%
+    map(
+      ~{Sys.sleep(1)
+        read_html(.) %>%
+          html_nodes(.,"a") %>%
+          html_attr(., "onclick") %>%
+          str_subset(., "setlist") %>%
+          str_match_all(., "[0-9]{8}") %>%
+          purrr::flatten_chr(.)
+      }
+    ) %>%
+    purrr::flatten_chr()
+  
+  # 既存DBとの差分のみを抽出
+  db_latest_chr <- db_latest %>%
+    as.character() %>%
+    str_remove_all("-")
+  
+  live_dates2 <- live_dates[live_dates > db_latest_chr]
+  
+  live_urls <- live_dates2 %>%
+    map_chr(~str_c("https://www.onmyo-za.net/",
+                   "setlist/",
+                   .x,
+                   ".html"
+    ))
+  
+  message("Extracting Setlists...")
+  # セットリストの取得
+  setlist_new <- list()
+  for (i in seq_len(length(live_urls))) {
+    Sys.sleep(2)
+    cat(live_dates2[i], "\n", sep = "")
+    tryCatch(
+      setlist_new[[i]] <- extract_setlist(live_urls[i]),
+      error = function(e){print("Failed to extract")}
+    )
+    
+  }
+  
+  df_setlist_new <- bind_rows(setlist_new)
+  
+  message("Saving New Data...")
+  saveRDS(df_setlist_new,
+          here::here('data',
+                     glue::glue('df_setlists_updated{today()}.rds')))
 }
--- global.R
+++ global.R
@@ -4,9 +4,14 @@ library(DT)
 library(leaflet)
 # library(ggmap)
 
+setlists_rds <- list.files(path = here::here('data'),
+                           pattern = 'setlists',
+                           full.names = T)
+
 df_dvd_songs <- read_rds('data/df_dvd_songs.rds') %>%
   filter(!str_detect(song, '収録'))
-df_setlists <- read_rds('data/df_setlists.rds')
+df_setlists <- setlists_rds %>%
+  map_dfr(read_rds)
 df_songs <- read_rds('data/df_songs.rds')
 
 dt_options <- list(lengthMenu = c(5, 10, 20, 50, 100, 200),
