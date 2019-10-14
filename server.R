f0255459913b73236c4fdf38bf2c8df2547807aa
Yuya Matsumura <mattu.yuya@gmail.com>
2019-10-14 23:10:49 +0900

    leafletをコメントアウト

--- server.R
+++ server.R
@@ -15,28 +15,28 @@ shinyServer(function(input, output) {
                                       extensions = c('Buttons'),
                                       options = dt_options_album)
   
-  # leaflet
-  values = reactiveValues(geocodes = data.frame(lng = 139.7545, lat = 35.70346))
-  
-  observeEvent(input$submit, {
-    geo1 = geocode(input$search_word1) %>%
-      as.data.frame() %>% as.vector()
-    
-    if(is.na(geo1[1, 1])){
-      geo1 = values$geocodes
-    }
-    
-    values$geocodes = geo1
-  })
-  
-  output$leafletplot = renderLeaflet({
-    geo1_lng <- values$geocodes[1,1]
-    geo1_lat <- values$geocodes[1,2]
-    
-    map_data <- leaflet() %>% 
-      addTiles() %>%
-      addMarkers(lng = geo1_lng, lat = geo1_lat,
-                 label = input$search_word1)
-    return(map_data)
-  })
+  # # leaflet
+  # values = reactiveValues(geocodes = data.frame(lng = 139.7545, lat = 35.70346))
+  # 
+  # observeEvent(input$submit, {
+  #   geo1 = geocode(input$search_word1) %>%
+  #     as.data.frame() %>% as.vector()
+  #   
+  #   if(is.na(geo1[1, 1])){
+  #     geo1 = values$geocodes
+  #   }
+  #   
+  #   values$geocodes = geo1
+  # })
+  # 
+  # output$leafletplot = renderLeaflet({
+  #   geo1_lng <- values$geocodes[1,1]
+  #   geo1_lat <- values$geocodes[1,2]
+  #   
+  #   map_data <- leaflet() %>% 
+  #     addTiles() %>%
+  #     addMarkers(lng = geo1_lng, lat = geo1_lat,
+  #                label = input$search_word1)
+  #   return(map_data)
+  # })
 })
